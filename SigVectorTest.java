5324071282e51f2bc0852d281c3d463615613247
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2021-10-16 17:53:10 -0400

    Add files via upload

--- SigVectorTest.java
+++ SigVectorTest.java
@@ -8,7 +8,7 @@ import java.util.zip.CRC32;
 
 public class SigVectorTest {
     
-    private static final int N = 512;
+    private static final int N = 42;
     private static final int SEED = 12345;
     private static final long[] keys = new long[N];
     // static block is executed once at class initialization
@@ -51,15 +51,20 @@ public class SigVectorTest {
         }
     }
     
+    @Test public void testEqualsHundredThousand() {
+        testEquals(100000, 350196673L);
+    }
+    
     @Test public void testEqualsTenThousand() {
-        testEquals(10000, 1102264136L);
+        testEquals(10000, 4189266982L);
     }
     
     @Test public void testEqualsHundred() {
-        testEquals(100, 3493624404L);
+        testEquals(100, 8143682L);
     }
     
     private void testEquals(int n, long expected) {
+        final int WINDOW = 20;
         Random rng = new Random(SEED);
         SigVector[] svs = new SigVector[n];
         int[] prev = new int[n];
@@ -68,7 +73,7 @@ public class SigVectorTest {
         svs[0] = new SigVector(N, keys);
         vectors.add(svs[0]);
         for(int i = 1; i < n; i++) {
-            int j = i < 21 ? rng.nextInt(i): i - 1 - rng.nextInt(20);
+            int j = i < WINDOW + 1 ? rng.nextInt(i): i - 1 - rng.nextInt(WINDOW);
             prev[i] = j;
             SigVector current = (SigVector)svs[j].clone();
             svs[i] = current;
@@ -79,11 +84,14 @@ public class SigVectorTest {
                 j = prev[j];
                 assertEquals(svs[j], current2);
             }
-            bit[i] = rng.nextInt(N);
+            bit[i] = rng.nextInt(10 + i % (N - 10));
+            if(bit[i] == bit[prev[i]]) {
+                bit[i] = (bit[i] + 1) % N;
+            }
             current.set(bit[i], !current.get(bit[i]));
             vectors.add(current);
         }
-        
+        // System.out.println(vectors.size());
         CRC32 check = new CRC32();
         for(int i = 0; i < n; i++) {
             assertTrue(vectors.contains(svs[i]));
