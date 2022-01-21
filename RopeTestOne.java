9e8452ea01034acd609ae0ec1f3724a9ccefd1fd
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2022-01-21 13:20:31 -0500

    Add files via upload

---
+++ RopeTestOne.java
@@ -0,0 +1,134 @@
+import org.junit.Test;
+import java.util.Random;
+import java.util.zip.CRC32;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class RopeTestOne {
+
+    @Test public void testSimpleKnownCases() {
+        String s1 = "hello";
+        String s2 = "world";
+
+        // Turning ordinary strings into ropes
+        Rope r1 = new StringRope(s1); // "hello"
+        Rope r2 = new StringRope(s2); // "world"
+        assertEquals(s1.length(), r1.length());
+        assertEquals(s1, r1.toString());
+        assertEquals('e', r1.charAt(1));
+        assertEquals('d', r2.charAt(4));
+
+        // Rope concatenation
+        Rope r3 = new ConcatRope(r1, r2); // "helloworld"
+        Rope r4 = new ConcatRope(r3, r3); // "helloworldhelloworld"
+        assertEquals(s1.length() + s2.length(), r3.length());
+        assertEquals(s1 + s2, r3.toString());
+        assertEquals(2 * r3.length(), r4.length());
+        assertEquals(s1 + s2 + s1 + s2, r4.toString());
+
+        // Subrope extraction
+        Rope r5 = new SubRope(r3, 2, 8); // llowor
+        assertEquals(8 - 2, r5.length());
+        assertEquals("llowor", r5.toString());
+        assertEquals('o', r5.charAt(2));
+        assertEquals('o', r5.charAt(4));
+
+        // Special case of slicing out an empty subrope.
+        Rope r6 = new SubRope(r4, 15, 15);
+        assertEquals(0, r6.length());
+        assertEquals("", r6.toString());
+        // Even from the empty rope, an empty subrope can be extracted.
+        Rope r7 = new SubRope(r6, 0, 0);
+        assertEquals(0, r7.length());
+        assertEquals("", r7.toString());
+    }
+
+    @Test public void testExceptions() {
+        Rope r1 = new StringRope("Learning the ropes");
+        testExceptions(r1);
+
+        Rope r2 = new ConcatRope(r1, r1); // "Learning the ropesLearning the ropes"
+        testExceptions(r2);
+
+        Rope r3 = new SubRope(r1, 4, 12); // "ning the"
+        testExceptions(r3);
+
+        Rope r4 = new ConcatRope(r1, r2);
+    }
+
+    private void testExceptions(Rope r) {
+        try { // Negative index must throw IndexOutOfBoundsException.
+            r.charAt(-1);
+            fail(); // This line must be unreachable
+        }
+        catch(IndexOutOfBoundsException ignored) {}
+        try { // Index greater or equal to length must throw IndexOutOfBoundsException.
+            r.charAt(r.length());
+            fail(); // This line must be unreachable
+        }
+        catch(IndexOutOfBoundsException ignored) {}
+    }
+
+    @Test public void testOneHundred() {
+        test(100, 4028914464L);
+    }
+
+    @Test public void testOneThousand() {
+        test(1000, 3273852294L);
+    }
+
+    @Test public void testOneMillion() {
+        test(1_000_000, 2206054464L);
+    }
+
+    private void test(int n, long expected) {
+        // RIP Meatloaf,  January 20, 2022
+        String[] initial = {
+                "Special olympians are the real heroes.",
+                "I'm gonna give 'em everything I've got.",
+                "But who's gonna help me?",
+                "We'll help, Meatloaf. But how?",
+                "By returning this coupon today!"
+        };
+        final int MAXLEN = 1_000_000_000;
+        Rope[] ropes = new Rope[n];
+        Random rng = new Random(12345 + n);
+        CRC32 check = new CRC32();
+        for(int i = 0; i < n; i++) {
+            if(i < initial.length) {
+                ropes[i] = new StringRope(initial[i]);
+            }
+            else {
+                Rope r1 = ropes[rng.nextInt(i)];
+                Rope r2 = ropes[rng.nextInt(i)];
+                // Concatenate the ropes 75% of the time, but only if the result length fits in an int.
+                if(rng.nextInt(100) < 75 && r1.length() < MAXLEN && r2.length() < MAXLEN) {
+                    ropes[i] = new ConcatRope(r1, r2);
+                }
+                else {
+                    int len = r1.length();
+                    int start, end;
+                    if(len > 0) {
+                        start = rng.nextInt(len);
+                        end = start + rng.nextInt(len - start);
+                    }
+                    else {
+                        end = start = 0;
+                    }
+                    ropes[i] = new SubRope(r1, start, end);
+                }
+            }
+            // System.out.println(i + ": (" + ropes[i].length() + ") <" + ropes[i] + ">");
+            int len = ropes[i].length();
+            // Since these ropes can get pretty long, we'll just randomly sample some characters.
+            int k = 0, step = len / 4 + 1;
+            while(k < len) {
+                check.update(ropes[i].charAt(k));
+                k += rng.nextInt(step) + 1;
+            }
+        }
+        assertEquals(expected, check.getValue());
+    }
+
+}
---
+++ RopeTestTwo.java
@@ -0,0 +1,144 @@
+import org.junit.Test;
+import java.util.Random;
+import java.util.zip.CRC32;
+import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class RopeTestTwo {
+
+    @Test public void testEqualsExplicit() {
+        Rope r1 = new StringRope("hello");
+        Rope r2 = new StringRope("world");
+        assertEquals(r1, r1);
+        assertEquals(r2, r2);
+        assertFalse(r1.equals(r2));
+        Rope r3 = new StringRope("helloworld");
+        Rope r4 = new ConcatRope(r1, r2);
+        assertEquals(r3, r4);
+        Rope r5 = new SubRope(r3, 3, 8);
+        Rope r6 = new StringRope("lowor");
+        assertEquals(r5, r6);
+        assertFalse(r1.equals(r6));
+        assertFalse(r2.equals(r6));
+    }
+
+    @Test public void testCompareExplicit() {
+        Rope r1 = new StringRope("hello");
+        Rope r2 = new StringRope("hella");
+        assertTrue(r1.compareTo(r2) > 0);
+        assertTrue(r2.compareTo(r1) < 0);
+        assertTrue(r1.compareTo(r1) == 0);
+        assertTrue(r2.compareTo(r2) == 0);
+
+        Rope r3 = new ConcatRope(r1, r2); // "hellohella"
+        Rope r4 = new ConcatRope(r2, r1); // "hellahello"
+        assertTrue(r1.compareTo(r3) < 0);
+        assertTrue(r2.compareTo(r3) < 0);
+        assertTrue(r3.compareTo(r1) > 0);
+        assertTrue(r3.compareTo(r2) > 0);
+        assertTrue(r3.compareTo(r4) > 0);
+        assertTrue(r4.compareTo(r3) < 0);
+
+        Rope r5 = new SubRope(r3, 0, 5); // "hello"
+        Rope r6 = new SubRope(r3, 5, 9); // "hell"
+        assertTrue(r5.compareTo(r6) > 0);
+        assertTrue(r6.compareTo(r5) < 0);
+        assertTrue(r1.compareTo(r5) == 0);
+        assertTrue(r1.compareTo(r6) > 0);
+        assertTrue(r2.compareTo(r6) > 0);
+    }
+
+    @Test public void massTestComparisonsHundred() {
+        massTestComparisons(100, 1600454116L);
+    }
+
+    @Test public void massTestComparisonsTenThousand() {
+        massTestComparisons(10_000, 2319695085L);
+    }
+
+    @Test public void massTestComparisonsHundredThousand() {
+        massTestComparisons(100_000, 2947062926L);
+    }
+
+    private void massTestComparisons(int n, long expected) {
+        Random rng = new Random(12345 + n);
+        CRC32 check = new CRC32();
+        Rope[] ropes = new Rope[n];
+        String[] initial = {
+                "aaaaa", "abab", "babb", "bbbaba"
+        };
+        final int MAXLEN = 1000; // Let's keep these rope lengths a bit shorter for this test.
+
+        HashSet<Rope> ropeHash = new HashSet<>();
+        TreeSet<Rope> ropeTree = new TreeSet<>();
+
+        for(int i = 0; i < n; i++) {
+            if(i < initial.length) {
+                ropes[i] = new StringRope(initial[i]);
+            }
+            else {
+                Rope r1 = ropes[rng.nextInt(i)];
+                Rope r2 = ropes[rng.nextInt(i)];
+                // Concatenate the ropes 50% of the time, but only if the result length fits in an int.
+                if(rng.nextInt(100) < 50 && r1.length() < MAXLEN && r2.length() < MAXLEN) {
+                    ropes[i] = new ConcatRope(r1, r2);
+                }
+                else {
+                    int len = r1.length();
+                    int start, end;
+                    if(len > 0) {
+                        start = rng.nextInt(len);
+                        end = start + rng.nextInt(len - start);
+                    }
+                    else {
+                        end = start = 0;
+                    }
+                    ropes[i] = new SubRope(r1, start, end);
+                }
+            }
+            ropeHash.add(ropes[i]);
+            ropeTree.add(ropes[i]);
+            // The sizes of two sets must be in lockstep at all times.
+            assertFalse(ropeHash.size() < ropeTree.size());
+            assertFalse(ropeHash.size() > ropeTree.size());
+            // Update the checksum based on some order comparisons.
+            if(i > 0) {
+                check.update(ropes[i].compareTo(ropes[i-1]) < 0 ? 42: 99);
+            }
+
+        }
+
+        // Build a HashMap of all the hash codes that occur for the previous ropes, and
+        // map each code to the list of ropes that have that hash code.
+        HashMap<Integer, ArrayList<Rope>> ropeMap = new HashMap<>();
+        for(Rope rope: ropes) {
+            assertTrue(ropeHash.contains(rope));
+            assertTrue(ropeTree.contains(rope));
+            int h = rope.hashCode();
+            if(!ropeMap.containsKey(h)) {
+                ropeMap.put(h, new ArrayList<Rope>());
+            }
+            ropeMap.get(h).add(rope);
+        }
+        // Verify that ropes with different hash codes are not equal to each other.
+        ArrayList<Rope> prev = null;
+        for(Integer h: ropeMap.keySet()) {
+            ArrayList<Rope> ropesHere = ropeMap.get(h);
+            for(Rope r1: ropesHere) {
+                if(prev != null) {
+                    for (Rope r2 : prev) {
+                        assertFalse(r1.equals(r2));
+                    }
+                }
+            }
+            prev = ropesHere;
+        }
+        assertEquals(expected, check.getValue());
+    }
+}
