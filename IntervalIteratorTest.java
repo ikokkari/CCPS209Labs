730446ce373e025b085d0da39346471ea477a35d
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2022-02-09 19:54:54 -0500

    Add files via upload

--- IntervalIteratorTest.java
+++ IntervalIteratorTest.java
@@ -1,8 +1,11 @@
 import org.junit.Test;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.zip.CRC32;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import java.util.ArrayList;
 
 public class IntervalIteratorTest {
 
@@ -23,8 +26,10 @@ public class IntervalIteratorTest {
         IntervalSet intervals = new IntervalSet();
         assertEquals("[]", extinguish(intervals.iterator()));
 
-        intervals.add(10, 15);
         intervals.add(17);
+        assertEquals("[17]", extinguish(intervals.iterator()));
+
+        intervals.add(10, 15);
         intervals.add(20, 24);
         assertEquals("[10, 11, 12, 13, 14, 15, 17, 20, 21, 22, 23, 24]", extinguish(intervals.iterator()));
 
@@ -35,15 +40,22 @@ public class IntervalIteratorTest {
         intervals.add(42);
         assertEquals("[3, 5, 6, 7, 8, 10, 11, 12, 13, 14, 42]", extinguish(intervals.iterator()));
 
-        // Last test using two iterators simultanously to the same IntervalSet.
+        // Last test using two simultaneous iterators to the previous IntervalSet.
         Iterator<Integer> iterator0 = intervals.iterator();
         Iterator<Integer> iterator1 = intervals.iterator();
         // Consume the first two elements from the first iterator.
         iterator0.next();
         iterator0.next();
         assertEquals("[3, 5, 6, 7, 8, 10, 11, 12, 13, 14, 42]", extinguish(iterator1));
-        // First two elements should not be there in the result.
+        // First two elements of the IntervalSet should not be in the result.
         assertEquals("[6, 7, 8, 10, 11, 12, 13, 14, 42]", extinguish(iterator0));
+
+        // Let's celebrate the fact that the IntervalSet is now an Iterable.
+        ArrayList<Integer> list = new ArrayList<>();
+        for(Integer e: intervals) { // Foreach-loop through the IntervalSet!
+            list.add(e);
+        }
+        assertEquals("[3, 5, 6, 7, 8, 10, 11, 12, 13, 14, 42]", list.toString());
     }
 
     @Test public void testIteratorHundred() {
@@ -87,5 +99,27 @@ public class IntervalIteratorTest {
             }
         }
         assertEquals(expected, check.getValue());
+    }
+
+    @Test public void testConcurrentModification() {
+        IntervalSet intervals = new IntervalSet();
+        intervals.add(42, 99);
+        Iterator<Integer> iterator = intervals.iterator();
+        iterator.next();
+        iterator.next();
+        intervals.add(10, 20);
+        try {
+            iterator.next();
+            fail(); // This line must be unreachable due to exception being thrown.
+        }
+        catch(ConcurrentModificationException ignored) {}
+        iterator = intervals.iterator();
+        iterator.next();
+        intervals.remove(11, 70);
+        try {
+            iterator.next();
+            fail(); // This line must be unreachable due to exception being thrown.
+        }
+        catch(ConcurrentModificationException ignored) {}
     }
 }
