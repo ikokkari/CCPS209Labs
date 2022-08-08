cc85f00c35f0592445383301900c8a4c96cab5a3
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2022-08-08 14:43:28 -0400

    Add files via upload

--- VarshamovTenengoltsTest.java
+++ VarshamovTenengoltsTest.java
@@ -11,7 +11,11 @@ public class VarshamovTenengoltsTest {
         assertEquals("11100", VarshamovTenengolts.encode("10"));
         assertEquals("10001", VarshamovTenengolts.encode("01"));
         assertEquals("001011", VarshamovTenengolts.encode("111"));
+        assertEquals("001011", VarshamovTenengolts.encode("111"));
+        assertEquals("1010001000", VarshamovTenengolts.encode("100100"));
         assertEquals("110011001001100", VarshamovTenengolts.encode("01101001100"));
+        assertEquals("0000001001000110111", VarshamovTenengolts.encode("00010100011111"));
+        assertEquals("001001000011111001000100110001001111", VarshamovTenengolts.encode("101000111110100010011000101111"));
     }
 
     @Test public void testDecodeExplicit() {
@@ -19,35 +23,48 @@ public class VarshamovTenengoltsTest {
         assertEquals("01", VarshamovTenengolts.decode("0001"));
         assertEquals("111", VarshamovTenengolts.decode("00011"));
         assertEquals("111", VarshamovTenengolts.decode("01011"));
-        assertEquals("01101001100", VarshamovTenengolts.decode("11001101001100"));
-        assertEquals("01101001100", VarshamovTenengolts.decode("11001100100110"));
+        assertEquals("100100", VarshamovTenengolts.decode("101001000"));
+        assertEquals("100100", VarshamovTenengolts.decode("101000000"));
+        assertEquals("01101001100", VarshamovTenengolts.decode("11001100101100"));
+        assertEquals("11001100000111", VarshamovTenengolts.decode("110100011000001111"));
+        assertEquals("110000011110100", VarshamovTenengolts.decode("0011100000011100100"));
+        assertEquals("101000111110100010011000101111", VarshamovTenengolts.decode("00100100001111001000100110001001111"));
     }
 
     @Test public void massTestOneThousand() {
-        massTest(1000);
+        massTest(546136691L,1000);
     }
 
     @Test public void massTestOneMillion() {
-        massTest(1000000);
+        massTest(3477739903L,1000000);
     }
 
-    private void massTest(int n) {
+    private void massTest(long expected, int n) {
         Random rng = new Random(12345 + n);
+        CRC32 check = new CRC32();
         int m = 3, goal = 10;
         for(int i = 0; i < n; i++) {
+            // Construct the random message of m bits for Alice.
             StringBuilder original = new StringBuilder();
             for(int j = 0; j < m; j++) {
                 original.append(rng.nextBoolean() ? '0' : '1');
             }
+            // Encode the original message for communication over single deletion channel.
             String encoded = VarshamovTenengolts.encode(original.toString());
-            // Adversary now removes the character in a random position.
+            for(int j = 0; j < encoded.length(); j++) {
+                check.update(encoded.charAt(j));
+            }
+            // Adversary now removes one character from a random position.
             int pos = rng.nextInt(m);
-            String adversary = encoded.substring(0, pos) + encoded.substring(pos + 1);
-            String decoded = VarshamovTenengolts.decode(adversary);
+            String received = encoded.substring(0, pos) + encoded.substring(pos + 1);
+            // Decode the received message, and verify that the result equals the original.
+            String decoded = VarshamovTenengolts.decode(received);
             assertEquals(original.toString(), decoded);
+            // Update the length of the generated random bit string.
             if(i == goal) {
                 goal = (3 * goal) / 2; m++;
             }
         }
+        assertEquals(expected, check.getValue());
     }
 }
