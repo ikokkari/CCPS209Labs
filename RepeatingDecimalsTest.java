90e18f69fae4a3acecd3e87dd501190131ba9a58
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2022-08-08 01:18:38 -0400

    Add files via upload

--- RepeatingDecimalsTest.java
+++ RepeatingDecimalsTest.java
@@ -8,6 +8,11 @@ import static org.junit.Assert.assertEquals;
 public class RepeatingDecimalsTest {
 
     @Test public void testExplicit() {
+
+        String[] r0 = RepeatingDecimals.decimals(5, 74);
+        String[] e0 = {"0.0", "675"};
+        assertArrayEquals(e0, r0);
+
         String[] r1 = RepeatingDecimals.decimals(1, 6);
         String[] e1 = {"0.1", "6"};
         assertArrayEquals(e1, r1);
@@ -32,9 +37,9 @@ public class RepeatingDecimalsTest {
         String[] e6 = {"0.", "157894736842105263"};
         assertArrayEquals(e6, r6);
 
-        String[] r7 = RepeatingDecimals.decimals(1, 44);
-        String[] e7 = {"0.02", "27"};
-        assertArrayEquals(e6, r6);
+        String[] r7 = RepeatingDecimals.decimals(1, 61);
+        String[] e7 = {"0.", "016393442622950819672131147540983606557377049180327868852459"};
+        assertArrayEquals(e7, r7);
 
         String[] r8 = RepeatingDecimals.decimals(1, 97);
         String[] e8 = {"0.", "010309278350515463917525773195876288659793814432989690721649484536082474226804123711340206185567"};
@@ -47,14 +52,39 @@ public class RepeatingDecimalsTest {
         String[] r10 = RepeatingDecimals.decimals(1, 256);
         String[] e10 = {"0.00390625", "0"};
         assertArrayEquals(e10, r10);
+
+        String[] r11 = RepeatingDecimals.decimals(70, 72);
+        String[] e11 = {"0.97", "2"};
+        assertArrayEquals(e11, r11);
+
+        // A cute one from https://www.youtube.com/watch?v=daro6K6mym8
+        String[] r12 = RepeatingDecimals.decimals(1, 998001);
+        StringBuilder allBut998 = new StringBuilder();
+        for(int i = 0; i < 1000; i++) {
+            if(i != 998) {
+                if(i < 10) { allBut998.append("00"); allBut998.append(i); }
+                else if(i < 100) { allBut998.append("0"); allBut998.append(i); }
+                else { allBut998.append(i); }
+            }
+        }
+        String[] e12 = {"0.", allBut998.toString()};
+        assertArrayEquals(e12, r12);
+
+        String[] r13 = RepeatingDecimals.decimals(20, 27);
+        String[] e13 = {"0.", "740"};
+        assertArrayEquals(e13, r13);
+
+        String[] r14 = RepeatingDecimals.decimals(5, 997);
+        String[] e14 = {"0.", "0050150451354062186559679037111334002006018054162487462387161484453360080240722166499498495486459378134403209628886659979939819458375125376128385155466399197592778335"};
+        assertArrayEquals(e14, r14);
     }
 
     @Test public void massTestHundred() {
-        massTest(100, 2300676283L);
+        massTest(100, 435856192L);
     }
 
     @Test public void massTestTenThousand() {
-        massTest(10000, 2465431047L);
+        massTest(10000, 2945858635L);
     }
 
     private void massTest(int n, long expected) {
@@ -64,7 +94,9 @@ public class RepeatingDecimalsTest {
         while(b < n) {
             int a = 1;
             while(a < b) {
+                //System.out.print(a + " " + b + " ");
                 String[] result = RepeatingDecimals.decimals(a, b);
+                //System.out.println(result[0] + " " + result[1]);
                 for(int i = 0; i < result[0].length(); i++) {
                     check.update(result[0].charAt(i));
                 }
@@ -73,7 +105,7 @@ public class RepeatingDecimalsTest {
                 }
                 a += rng.nextInt(b - a + 2) + 1;
             }
-            b++;
+            b += rng.nextInt(4) + 1;
         }
         assertEquals(expected, check.getValue());
     }
