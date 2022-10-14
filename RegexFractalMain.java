6a904d7b04cd551815ed0949d9936a2ad2d0de0c
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2022-10-14 04:42:05 -0400

    Add files via upload

---
+++ RegexFractalMain.java
@@ -0,0 +1,43 @@
+import javax.swing.BorderFactory;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class RegexFractalMain {
+
+    private static class ImagePanel extends JPanel {
+        private final Image img;
+        public ImagePanel(Image img, String title) {
+            this.img = img;
+            this.setToolTipText(title);
+            this.setPreferredSize(new Dimension(img.getWidth(this), img.getHeight(this)));
+            this.setBorder(BorderFactory.createEtchedBorder());
+        }
+        public void paintComponent(Graphics g) {
+            g.drawImage(img, 0, 0, this);
+        }
+    }
+
+    private static final String[] REGEXES = {
+        ".*(13|31|24|42).*", ".*[^4][^32]3.*", "[34]*2.*", "[124]*3[24]*(3.*)?"
+    };
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Regular Expression Fractals");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setLayout(new GridLayout(2, 4));
+        for(String regex: REGEXES) {
+            BufferedImage resultF = RegexFractal.render(regex, 256, false);
+            frame.add(new ImagePanel(resultF, regex + ", no rotate"));
+            BufferedImage resultT = RegexFractal.render(regex, 256, true);
+            frame.add(new ImagePanel(resultT, regex + ", rotate"));
+
+        }
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
