fd5b2a2f620b17d6758407bea8940258d9a15e1e
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2022-10-14 05:10:16 -0400

    Add files via upload
    Added two more example fractals that utilize some lookahead and lookbehind capabilities of Java regular expressions.

--- RegexFractalMain.java
+++ RegexFractalMain.java
@@ -23,18 +23,20 @@ public class RegexFractalMain {
     }
 
     private static final String[] REGEXES = {
-        ".*(13|31|24|42).*", ".*[^4][^32]3.*", "[34]*2.*", "[124]*3[24]*(3.*)?"
+            ".*(13|31|24|42).*", ".*[^4][^32]3.*",
+            "[34]*2.*", "[124]*3[24]*(3.*)?",
+            ".*(12|(2(?![34]))).*", ".*(13|32|((?<!4)24)).*(?!\\1)$"
     };
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Regular Expression Fractals");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setLayout(new GridLayout(2, 4));
+        frame.setLayout(new GridLayout(3, 4));
         for(String regex: REGEXES) {
             BufferedImage resultF = RegexFractal.render(regex, 256, false);
-            frame.add(new ImagePanel(resultF, regex + ", no rotate"));
+            frame.add(new ImagePanel(resultF, regex + " with no rotate"));
             BufferedImage resultT = RegexFractal.render(regex, 256, true);
-            frame.add(new ImagePanel(resultT, regex + ", rotate"));
+            frame.add(new ImagePanel(resultT, regex + " with rotate"));
 
         }
         frame.pack();
