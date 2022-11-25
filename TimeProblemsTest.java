1e553ac849d606011456eae263fe1a075a811be5
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2022-11-25 10:14:31 -0500

    Add files via upload
    Added some explicit test cases for countFridayTheThirteens problem.

--- TimeProblemsTest.java
+++ TimeProblemsTest.java
@@ -14,12 +14,46 @@ public class TimeProblemsTest {
             0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
         };
 
-    @Test public void testCountFridayThirteens() {
-        Random rng = new Random(12345);
+    @Test public void testCountFridayThirteensExplicit() {
+        LocalDate startDate1 = LocalDate.of(1905, 4, 13);
+        LocalDate endDate1 = LocalDate.of(1905, 11, 9);
+        assertEquals(1, TimeProblems.countFridayThirteens(startDate1, endDate1));
+
+        LocalDate startDate2 = LocalDate.of(1905, 4, 13);
+        LocalDate endDate2 = LocalDate.of(1905, 11, 9);
+        assertEquals(1, TimeProblems.countFridayThirteens(startDate2, endDate2));
+
+        LocalDate startDate3 = LocalDate.of(1912, 12, 21);
+        LocalDate endDate3 = LocalDate.of(1956, 12, 22);
+        assertEquals(75, TimeProblems.countFridayThirteens(startDate3, endDate3));
+
+        LocalDate startDate4 = LocalDate.of(1901, 9, 13);
+        LocalDate endDate4 = LocalDate.of(1912, 01, 13);
+        assertEquals(19, TimeProblems.countFridayThirteens(startDate4, endDate4));
+
+        LocalDate startDate5 = LocalDate.of(2187, 6, 29);
+        LocalDate endDate5 = LocalDate.of(2283, 4, 13);
+        assertEquals(166, TimeProblems.countFridayThirteens(startDate5, endDate5));
+
+        LocalDate startDate6= LocalDate.of(1936, 4, 17);
+        LocalDate endDate6 = LocalDate.of(1941, 6, 13);
+        assertEquals(8, TimeProblems.countFridayThirteens(startDate6, endDate6));
+
+        LocalDate startDate7= LocalDate.of(1982, 8, 13);
+        LocalDate endDate7 = LocalDate.of(2012, 12, 21);
+        assertEquals(53, TimeProblems.countFridayThirteens(startDate7, endDate7));
+
+        LocalDate startDate8= LocalDate.of(2164, 4, 13);
+        LocalDate endDate8 = LocalDate.of(2337, 8, 13);
+        assertEquals(301, TimeProblems.countFridayThirteens(startDate8, endDate8));
+    }
+
+    @Test public void testCountFridayThirteensMass() {
+        Random rng = new Random(4242);
         CRC32 check = new CRC32();
-        for(int i = 0; i < 1000; i++) {
-            int y1 = 1900 + rng.nextInt(2000);
-            int y2 = y1 + rng.nextInt(i + 3);
+        for(int i = 0; i < 3000; i++) {
+            int y1 = 1900 + rng.nextInt(1 + (i+2)/5);
+            int y2 = y1 + rng.nextInt(3 + i/10);
             int m1 = rng.nextInt(12) + 1;
             int m2 = rng.nextInt(12) + 1;
             int d1 = rng.nextInt(100) < 20 ? 13 : rng.nextInt(daysInMonth[m1]) + 1;
@@ -30,10 +64,9 @@ public class TimeProblemsTest {
                 LocalDate tmp = startDate; startDate = endDate; endDate = tmp;
             }
             int result = TimeProblems.countFridayThirteens(startDate, endDate);
-            //System.out.println(startDate + " " + endDate + " " + result);
             check.update(result);
         }
-        assertEquals(2411428835L, check.getValue());
+        assertEquals(3994495565L, check.getValue());
     }
 
     @Test public void testDayAfterSeconds() {
